2cd9d3c6244c5442f13bddbbbb85eca26ea6c6c4
Anna-Anka <anjbekrenewa@gmail.com>
2024-11-15 20:04:21 +0300

    refactor council block in council page

--- src/js/libs/_mixitup.js
+++ src/js/libs/_mixitup.js
@@ -1,11 +1,14 @@
 import mixitup from 'mixitup';
-let mixer = mixitup('.releases-page');
 
-const select = document.getElementById('releases-years')
-if (select) {
-    select.addEventListener('change', function () {
-        var filterValue = this.options[this.selectedIndex].getAttribute('data-filter');
-        mixer.filter(filterValue);
-    });
+if (document.querySelector('.releases-page')) {
+    let mixer = mixitup('.releases-page');
 
+    const select = document.getElementById('releases-years')
+    if (select) {
+        select.addEventListener('change', function () {
+            var filterValue = this.options[this.selectedIndex].getAttribute('data-filter');
+            mixer.filter(filterValue);
+        });
+
+    }
 }
