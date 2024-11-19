8781fee29ac873d02ad5559234de9ca22562ed01
Anna-Anka <anjbekrenewa@gmail.com>
2024-11-19 18:51:29 +0300

    add article page

--- src/js/libs/_mixitup.js
+++ src/js/libs/_mixitup.js
@@ -1,18 +1,54 @@
 import mixitup from 'mixitup';
+import Choices from 'choices.js'
 
-if (document.querySelector('.releases-page')) {
-    let mixer = mixitup('.releases-page');
+const applyMixitupWithSelect = (containerClass, selectId) => {
+    if (document.querySelector(containerClass)) {
+        const select = document.getElementById(selectId)
+        // let choicesSelect;
 
-    const select = document.getElementById('releases-years')
-    if (select) {
-        select.addEventListener('change', function () {
-            var filterValue = this.options[this.selectedIndex].getAttribute('data-filter');
-            mixer.filter(filterValue);
+        // if (select && select.hasAttribute('data-choice')) {
+        //     choicesSelect = new Choices(select, {
+        //         searchEnabled: false,
+        //         searchChoices: false,
+        //         shouldSort: false,
+        //     });
+        // }
+
+        let mixer = mixitup(containerClass, {
+            // callbacks: {
+            //     onMixStart: () => {
+            //         if (select) {
+            //             let activeValue = ''
+            //             const options = Array.from(select.options)
+            //             options.forEach((option) => {
+            //                 // option.removeAttribute('selected');
+            //                 // option.removeAttribute('aria-selected');
+
+            //                 if (option.classList.contains('mixitup-control-active')) {
+            //                     // option.setAttribute('selected', true)
+            //                     // option.setAttribute('aria-selected', true)
+            //                     activeValue = option.value
+            //                 }
+            //             })
+            //             select.value = activeValue
+
+            //             if (choicesSelect) {
+            //                 choicesSelect.setChoiceByValue([activeValue])
+            //             }
+            //         }
+            //     }
+            // }
         });
 
+
+        if (select) {
+            select.addEventListener('change', function () {
+                const filterValue = this.options[this.selectedIndex].getAttribute('data-filter');
+                mixer.filter(filterValue);
+            });
+        }
     }
 }
 
-if (document.querySelector('.release-page')) {
-    mixitup('.release-page');
-}
+applyMixitupWithSelect('.releases-page', 'releases-years');
+applyMixitupWithSelect('.release-page', 'release-category');
