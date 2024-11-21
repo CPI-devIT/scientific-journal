3a203436c0c5a5ef694c19cf0c26f2af9df0867c
Anna-Anka <anjbekrenewa@gmail.com>
2024-11-21 18:13:21 +0300

    refactor select and mixitup

--- src/js/libs/_choices.js
+++
@@ -1,9 +0,0 @@
-import Choices from 'choices.js'
-const selects = document.querySelectorAll('[data-choice]');
-selects.forEach((select) => {
-    new Choices(select, {
-        searchEnabled: false,
-        searchChoices: false,
-        shouldSort: false,
-    });
-});
